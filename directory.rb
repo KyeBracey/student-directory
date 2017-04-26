1bf6ca59148b654e02eab4cdabc3a930eac8d4bb
kye.bracey@gmail.com <kye.bracey@gmail.com>
2017-04-26 17:37:13 +0000

    Finish all exercises (undo those which make the code less efficient/elegant)

--- directory.rb
+++ directory.rb
@@ -1,29 +1,77 @@
 def input_students
+    students = []
+    cohorts = [
+        :january, :february, :march, :april,
+        :may, :june, :july, :august, :september,
+        :october, :november, :december
+        ]
+    
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    students = []
     name = gets.chomp
     while !name.empty? do
-        students << {name: name, cohort: :november}
+        students << {name: name}
+        puts "Which cohort is #{name} in?"
+        cohort = gets.chomp.downcase.to_sym
+            if cohorts.include?(cohort)
+                students[(students.count-1)][:cohort] = cohort
+            else
+                puts "Input not recognised - defaulting to November"
+                students[(students.count-1)][:cohort] = :november
+            end
+        puts "Enter #{name}'s nationality:"
+        nationality = gets.chomp
+        students[(students.count-1)][:nationality] = nationality
+        puts "Enter #{name}'s height:"
+        height = gets.chomp
+        students[(students.count-1)][:height] = height
+        
+        puts "Student name: #{name}. Cohort: #{cohort}. Nationality: #{nationality}. Height: #{height}."
+        puts "Is this correct? (Y/N)"
+        answer = gets.chomp.downcase
+        unless answer == "y"
+            puts "Removing data. Please enter student's name again."
+            students.pop
+        end
         puts "Now we have #{students.count} students"
         name = gets.chomp
     end
-    students
+    students.sort_by!{|student| student[:cohort]}
+end
+
+def s_or_p(students)
+    return "student" if students.count == 1
+    return "students"
 end
 
 def print_header
-    puts "The students of Villians Academy"
-    puts "-------------"
+    puts "The students of Villians Academy".center(50)
+    puts "-------------".center(50)
 end
 
 def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    if students.count >= 1
+        existing_cohorts = students.collect{|student| student[:cohort]}.uniq
+        existing_cohorts.each do |cohort|
+            puts "#{cohort.capitalize} cohort:".center(50)
+            puts "-------------".center(50)
+            students.each do |student|
+                if student[:cohort] == cohort
+                    puts "#{student[:name]}".center(50)
+                    puts "Nationality: #{student[:nationality]}.  Height: #{student[:height]}.".center(50)#
+                end
+            end
+            puts "-------------".center(50)
+        end
     end
 end
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students."
+    if students.count >= 1
+        puts "Overall, we have #{students.count} great #{s_or_p(students)}.".center(50)
+    else
+        puts "We have no students!".center(50)
+    end
 end
 
 students = input_students
