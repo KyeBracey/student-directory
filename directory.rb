6f8d89f0a4a0c4d2f97dcfc55bcccd352c8e0096
kye.bracey@gmail.com <kye.bracey@gmail.com>
2017-04-27 15:11:45 +0000

    Add save_students method to save student list to students.csv

--- directory.rb
+++ directory.rb
@@ -12,6 +12,8 @@ def process(selection)
         input_students
     when "2"
         show_students
+    when "3"
+        save_students
     when "9"
         exit
     else
@@ -22,6 +24,7 @@ end
 def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
+    puts "3. Save the list to students.csv"
     puts "9. Exit the program"
 end
 
@@ -30,8 +33,6 @@ def show_students
     print_students_list
     print_footer
 end
-
-
 
 def input_students
     cohorts = [
@@ -71,6 +72,16 @@ def input_students
         name = gets.chomp
     end
     @students.sort_by!{|student| student[:cohort]}
+end
+
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+        student_data = [student[:name], student[:cohort]]
+        csv_line = student_data.join(",")
+        file.puts csv_line
+    end
+    file.close
 end
 
 def s_or_p
