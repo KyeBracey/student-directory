d58fd3afb86b595a7f2235603576a219cb424c6c
kye.bracey@gmail.com <kye.bracey@gmail.com>
2017-04-27 13:57:07 +0000

    Slightly refactor directory.rb - add show_students method, process method, rename print method to print_students_list.

--- directory.rb
+++ directory.rb
@@ -1,27 +1,39 @@
+@students = []
 def interactive_menu
-    students = []
     loop do
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9. Exit the program"
-        selection = gets.chomp
-        case selection
-        when "1"
-            students = input_students
-        when "2"
-            print_header
-            print(students)
-            print_footer(students)
-        when "9"
-            exit
-        else
-            puts "I'm not sure what you meant. Please try again."
-        end
+        print_menu
+        process(gets.chomp)
     end
 end
 
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "9"
+        exit
+    else
+        puts "I'm not sure what you meant. Please try again."
+    end
+end
+
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit the program"
+end
+
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end
+
+
+
 def input_students
-    students = []
     cohorts = [
         :january, :february, :march, :april,
         :may, :june, :july, :august, :september,
@@ -32,37 +44,37 @@ def input_students
     puts "To finish, just hit return twice"
     name = gets.chomp
     while !name.empty? do
-        students << {name: name}
+        @students << {name: name}
         puts "Which cohort is #{name} in?"
         cohort = gets.chomp.downcase.to_sym
             if cohorts.include?(cohort)
-                students[(students.count-1)][:cohort] = cohort
+                @students[(@students.count-1)][:cohort] = cohort
             else
                 puts "Input not recognised - defaulting to November"
-                students[(students.count-1)][:cohort] = :november
+                @students[(@students.count-1)][:cohort] = :november
             end
         puts "Enter #{name}'s nationality:"
         nationality = gets.chomp
-        students[(students.count-1)][:nationality] = nationality
+        @students[(@students.count-1)][:nationality] = nationality
         puts "Enter #{name}'s height:"
         height = gets.chomp
-        students[(students.count-1)][:height] = height
+        @students[(@students.count-1)][:height] = height
         
         puts "Student name: #{name}. Cohort: #{cohort}. Nationality: #{nationality}. Height: #{height}."
         puts "Is this correct? (Y/N)"
         answer = gets.chomp.downcase
         unless answer == "y"
             puts "Removing data. Please enter student's name again."
-            students.pop
+            @students.pop
         end
-        puts "Now we have #{students.count} students"
+        puts "Now we have #{@students.count} students"
         name = gets.chomp
     end
-    students.sort_by!{|student| student[:cohort]}
+    @students.sort_by!{|student| student[:cohort]}
 end
 
-def s_or_p(students)
-    return "student" if students.count == 1
+def s_or_p
+    return "student" if @students.count == 1
     return "students"
 end
 
@@ -71,13 +83,13 @@ def print_header
     puts "-------------".center(50)
 end
 
-def print(students)
-    if students.count >= 1
-        existing_cohorts = students.collect{|student| student[:cohort]}.uniq
+def print_students_list
+    if @students.count >= 1
+        existing_cohorts = @students.collect{|student| student[:cohort]}.uniq
         existing_cohorts.each do |cohort|
             puts "#{cohort.capitalize} cohort:".center(50)
             puts "-------------".center(50)
-            students.each do |student|
+            @students.each do |student|
                 if student[:cohort] == cohort
                     puts "#{student[:name]}".center(50)
                     puts "Nationality: #{student[:nationality]}.  Height: #{student[:height]}.".center(50)#
@@ -88,9 +100,9 @@ def print(students)
     end
 end
 
-def print_footer(students)
-    if students.count >= 1
-        puts "Overall, we have #{students.count} great #{s_or_p(students)}.".center(50)
+def print_footer
+    if @students.count >= 1
+        puts "Overall, we have #{@students.count} great #{s_or_p}.".center(50)
     else
         puts "We have no students!".center(50)
     end
